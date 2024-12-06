b3b4ace24f250b0d4b06e94a4e0a8f1698eb8322
brivaro <brian.1613.bv@gmail.com>
2024-12-06 14:57:08 +0100

    Version beta

--- api/chat.go
+++ api/chat.go
@@ -5,27 +5,22 @@ import (
 	"log"        
 	"time" 
     "context"  
-	"chat/initializers"    
+	"chat/initializers" 
+    "strconv"
+    "math/rand"
 
 	"github.com/nats-io/nats.go"
     "github.com/nats-io/nats.go/jetstream"
 )
 
 func SubscribeToChannel(channel string) {
-    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-    defer cancel()
-
-    subscription, err := initializers.JS.Subscribe(channel, func(msg jetstream.Msg) {
+    _, err := initializers.Client.Conn.Subscribe(channel, func(msg *nats.Msg) {
         log.Printf(string(msg.Data))
-    }, jetstream.Durable("durable-subscriber"), jetstream.ManualAck())
+    })
 
     if err != nil {
         log.Fatalf("Error al suscribirse al canal %s: %v", channel, err)
     }
-
-    // Mantener activa la suscripción
-    <-ctx.Done()
-    subscription.Unsubscribe()
 }
 
 func PublishMessage(channel, user, message string) {
@@ -35,7 +30,12 @@ func PublishMessage(channel, user, message string) {
     subject := fmt.Sprintf(channel)
     fullMessage := fmt.Sprintf("[%s]: %s", user, message)
 
-    _, err := initializers.JS.Publish(ctx, channel, []byte(fullMessage))
+    err := initializers.Client.Conn.Publish(subject, []byte(fullMessage))
+    if err != nil {
+        log.Fatalf("Error al publicar mensaje en el canal %s: %v", channel, err)
+    }
+
+    _, err = initializers.JS.Publish(ctx, subject, []byte(fullMessage))
     if err != nil {
         log.Fatalf("Error al publicar mensaje en el canal %s: %v", channel, err)
     }
@@ -47,32 +47,25 @@ func FetchRecentMessages(channel string) {
 
     subject := fmt.Sprintf(channel) //("chat.%s", channel)
 
-    startTime := time.Now().Add(-1 * time.Hour)
+    //startTime := time.Now().Add(-1 * time.Hour)
+    randId := rand.Intn(10) + 1
 
     // Config consumer
     consumer, err := initializers.ChatStream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
-        Name:          "consumer",
+        Name:          fmt.Sprintf("consumer_%s", strconv.Itoa(randId)),
         Durable:       "recent-messages",
         Description:   "Consumer to fetch recent messages",
-        FilterSubject: subject,                             // Filtro para el canal específico
-        DeliverPolicy: jetstream.DeliverByStartTimePolicy, // Inicia desde una hora específica
-        OptStartTime:  &startTime,                        // Hora de inicio
-        AckPolicy:     jetstream.AckNonePolicy,           // Política de recepción: No consume los mensajes
-        ReplayPolicy:  jetstream.ReplayInstantPolicy,     // Política de Reproducción: Reproducción instantánea
+        FilterSubject:  subject,                             // Filtro para el canal específico
+        //DeliverPolicy: jetstream.DeliverByStartTimePolicy, // Inicia desde una hora específica
+        //OptStartTime:  &startTime,                        // Hora de inicio
+        //AckPolicy:     jetstream.AckNonePolicy,           // Política de recepción: No consume los mensajes
+        //ReplayPolicy:  jetstream.ReplayInstantPolicy,     // Política de Reproducción: Reproducción instantánea
     })
-    // Creating/updating consumer
-    if err != nil {
-        log.Fatalf("Error creando consumidor: %v", err)
-    }
-    log.Println("Consumer created to recovery history mss.")
 
-    msgs, err := consumer.Fetch(10)
-    if err != nil {
-        // handle error
-    } else {
-        for msg := range msgs.Messages() {
-            fmt.Printf("Received a JetStream message: %s\n", string(msg.Data()))
-        }
-    }
-    log.Println("History mss finished!")
+	log.Printf("Created consumer")
+	if err != nil {
+		log.Fatal(err)
+	}
+    
+    log.Println("Created consumer", consumer.CachedInfo().Name)
 }
