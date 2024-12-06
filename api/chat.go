2713e808190b25e2b88d3fca738fdc5581795111
brivaro <brian.b.v.r.65@gmail.com>
2024-12-06 13:00:42 +0100

    correcciones

--- api/chat.go
+++ api/chat.go
@@ -12,21 +12,33 @@ import (
 )
 
 func SubscribeToChannel(channel string) {
-    subject := fmt.Sprintf(channel)
-    _, err := initializers.Client.Conn.Subscribe(subject, func(msg *nats.Msg) {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    subscription, err := initializers.JS.Subscribe(channel, func(msg jetstream.Msg) {
         log.Printf(string(msg.Data))
-    })
+    }, jetstream.Durable("durable-subscriber"), jetstream.ManualAck())
+
     if err != nil {
-        log.Fatalf("Error subscribing channel %s: %v", channel, err)
+        log.Fatalf("Error al suscribirse al canal %s: %v", channel, err)
     }
+
+    // Mantener activa la suscripción
+    <-ctx.Done()
+    subscription.Unsubscribe()
 }
 
 func PublishMessage(channel, user, message string) {
     ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
+
     subject := fmt.Sprintf(channel)
     fullMessage := fmt.Sprintf("[%s]: %s", user, message)
-    initializers.JS.Publish(ctx, subject, []byte(fullMessage))
+
+    _, err := initializers.JS.Publish(ctx, channel, []byte(fullMessage))
+    if err != nil {
+        log.Fatalf("Error al publicar mensaje en el canal %s: %v", channel, err)
+    }
 }
 
 func FetchRecentMessages(channel string) {
@@ -54,13 +66,14 @@ func FetchRecentMessages(channel string) {
     }
     log.Println("Consumer created to recovery history mss.")
 
-    _, err = consumer.Consume(func(msg jetstream.Msg) {
-        fmt.Printf(string(msg.Data()))
-        // No se envía ACK para mantener los mensajes disponibles
-    })
+    msgs, err := consumer.Fetch(10)
     if err != nil {
-        log.Fatalf("Error consuming mss: %v", err)
+        // handle error
     }
-
+    else{
+        for msg := range msgs.Messages() {
+            fmt.Printf("Received a JetStream message: %s\n", string(msg.Data()))
+        }
+    }
     log.Println("History mss finished!")
 }
